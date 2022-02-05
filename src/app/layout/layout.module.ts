5c5437508669f427e9d47411ebbc160da8934c99
hao <huynhtamhao@gmail.com>
2022-02-05 10:58:22 +0700

    feat(structure): add some config

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from '@layout/header/header.component';
+import { SidebarComponent } from '@layout/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    SidebarComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule
