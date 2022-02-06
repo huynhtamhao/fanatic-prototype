ce3be44495f44a06f2622df7e68c748631ffedb6
hao <huynhtamhao@gmail.com>
2022-02-06 11:31:59 +0700

    feat(struture): edit some config

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslocoRootModule } from './transloco/transloco-root.module';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once.guard';
 
 
 
@@ -16,4 +17,9 @@ import { SidebarComponent } from './layout/sidebar/sidebar.component';
     TranslocoRootModule,
   ]
 })
-export class CoreModule { }
+export class CoreModule extends EnsureModuleLoadedOnceGuard {
+  // Looks for the module in the parent injector to see if it's already been loaded (only want it loaded once)
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    super(parentModule);
+  }
+}
