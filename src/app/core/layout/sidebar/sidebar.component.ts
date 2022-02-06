1c09656e38429610fc7afa07bfa76c6de0b2e152
hao <huynhtamhao@gmail.com>
2022-02-06 11:01:59 +0700

    feat(sidebar): add sidebar

--- src/app/core/layout/sidebar/sidebar.component.ts
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,15 +1,140 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @angular-eslint/component-selector */
+import { Router, NavigationEnd } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  Inject,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  HostListener,
+  OnDestroy,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ROUTES } from './sidebar-items';
+import { RouteInfo } from './sidebar.metadata';
 
 @Component({
   selector: 'karios-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent  implements OnInit, OnDestroy {
+  public sidebarItems: RouteInfo[] | undefined;
+  level1Menu = '';
+  level2Menu = '';
+  level3Menu = '';
+  public innerHeight: any;
+  public bodyTag: any;
+  listMaxHeight: string | undefined;
+  listMaxWidth: string | undefined;
+  userFullName: string | undefined;
+  userImg: string | undefined;
+  userType: string | undefined;
+  headerHeight = 60;
+  routerObj: Subscription;
+  currentRoute: string | undefined;
+  currentUser: string | undefined;
+  currentUserAvatar: string | undefined;
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    public elementRef: ElementRef,
+    private router: Router
+  ) {
+    this.routerObj = this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        // logic for select active menu in dropdown
+        const currentUrl = event.url.split('?')[0];
+        this.level1Menu = currentUrl.split('/')[1];
+        this.level2Menu = currentUrl.split('/')[2];
 
-  constructor() { }
-
-  ngOnInit(): void {
+        // close sidebar on mobile screen after menu select
+        this.renderer.removeClass(this.document.body, 'overlay-open');
+      }
+    });
   }
-
+  @HostListener('window:resize', ['$event'])
+  windowResizeCall(event: Event) {
+    this.setMenuHeight();
+    this.checkStatusForResize(false);
+  }
+  @HostListener('document:mousedown', ['$event'])
+  onGlobalClick(event: any): void {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.renderer.removeClass(this.document.body, 'overlay-open');
+    }
+  }
+  callLevel1Toggle(event: any, element: any) {
+    if (element === this.level1Menu) {
+      this.level1Menu = '0';
+    } else {
+      this.level1Menu = element;
+    }
+    const hasClass = event.target.classList.contains('toggled');
+    if (hasClass) {
+      this.renderer.removeClass(event.target, 'toggled');
+    } else {
+      this.renderer.addClass(event.target, 'toggled');
+    }
+  }
+  callLevel2Toggle(event: any, element: any) {
+    if (element === this.level2Menu) {
+      this.level2Menu = '0';
+    } else {
+      this.level2Menu = element;
+    }
+  }
+  callLevel3Toggle(event: any, element: any) {
+    if (element === this.level3Menu) {
+      this.level3Menu = '0';
+    } else {
+      this.level3Menu = element;
+    }
+  }
+  ngOnInit() {
+    this.sidebarItems = ROUTES.filter((sidebarItem) => sidebarItem);
+    this.currentUserAvatar = "assets/images/usrbig.png";
+    this.initLeftSidebar();
+    this.bodyTag = this.document.body;
+  }
+  ngOnDestroy() {
+    this.routerObj.unsubscribe();
+  }
+  initLeftSidebar() {
+    const _this = this;
+    // Set menu height
+    _this.setMenuHeight();
+    _this.checkStatusForResize(true);
+  }
+  setMenuHeight() {
+    this.innerHeight = window.innerHeight;
+    const height = this.innerHeight - this.headerHeight;
+    this.listMaxHeight = height + '';
+    this.listMaxWidth = '500px';
+  }
+  isOpen() {
+    return this.bodyTag.classList.contains('overlay-open');
+  }
+  checkStatusForResize(firstTime: boolean = false) {
+    if (window.innerWidth < 1170) {
+      this.renderer.addClass(this.document.body, 'ls-closed');
+    } else {
+      this.renderer.removeClass(this.document.body, 'ls-closed');
+    }
+  }
+  mouseHover(e: any) {
+    const body = this.elementRef.nativeElement.closest('body');
+    if (body.classList.contains('submenu-closed')) {
+      this.renderer.addClass(this.document.body, 'side-closed-hover');
+      this.renderer.removeClass(this.document.body, 'submenu-closed');
+    }
+  }
+  mouseOut(e: any) {
+    const body = this.elementRef.nativeElement.closest('body');
+    if (body.classList.contains('side-closed-hover')) {
+      this.renderer.removeClass(this.document.body, 'side-closed-hover');
+      this.renderer.addClass(this.document.body, 'submenu-closed');
+    }
+  }
 }
