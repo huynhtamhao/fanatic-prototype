138d8cc50ce0924fab00d2bb7c937e31b21e3187
hao <huynhtamhao@gmail.com>
2022-02-06 10:49:06 +0700

    feat(structure): add structure directories

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [];
 
@@ -7,4 +8,8 @@ const routes: Routes = [];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {
+  static components = [
+    DashboardComponent,
+  ] 
+ }
