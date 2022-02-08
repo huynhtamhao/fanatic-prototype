6698a0c1e2e4a1c1aed9d6443c3717463943adb6
Le Hoang Quan <quan-lh@fanatic.vn>
2022-02-08 16:39:03 +0700

    feat(header): add header

--- src/app/core/constants/Constants.ts
+++ src/app/core/constants/Constants.ts
@@ -7,4 +7,5 @@ export abstract class Constants {
     WARNING: 'warning',
   };
 
+  static readonly timezoneJP = 'GMT +9';
 }
--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map, share, Subscription, timer } from 'rxjs';
+import { Constants } from '../../constants/Constants';
 
 @Component({
   selector: 'kairos-header',
@@ -6,13 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  timezoneJP = Constants.timezoneJP;
 
-  constructor() {
-    // Empty
-   }
+  public currentDate = new Date();
+  public currentTime = this.currentDate;
+  private midnight = new Date(this.currentDate.setHours(0, 0, 0, 0));
+  private lastNight = this.midnight.setDate(this.midnight.getDate() + 1);
+  private subscription = new Subscription();
+
+  constructor() { }
 
   ngOnInit(): void {
-    console.log("Comment avoid error");
+    // time annotation
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        this.currentTime = time;
+        if (this.currentTime.getTime() === this.lastNight) {
+          this.setTime();
+        }
+      });
+  }
+
+  // next date
+  setTime() {
+    this.currentDate = new Date();
+    this.midnight = new Date(this.currentDate.setHours(0, 0, 0, 0));
+    this.lastNight = this.midnight.setDate(this.midnight.getDate() + 1);
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
