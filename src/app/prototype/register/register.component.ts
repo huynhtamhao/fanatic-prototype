3607d3b75455cf40474b61aec185de10e760b6a5
Le Hoang Quan <quan-lh@fanatic.vn>
2022-02-09 15:51:52 +0700

    feat(form-register): add form register

--- src/app/prototype/register/register.component.ts
+++ src/app/prototype/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'kairos-register',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  form: FormGroup = new FormGroup({
+    productCd: new FormControl(null, [Validators.required, Validators.maxLength(20), Validators.minLength(6)]),
+    serialNo: new FormControl(null, [Validators.maxLength(20)]),
+    invoiceNo: new FormControl(null, [Validators.required, Validators.maxLength(11)]),
+    storageLocation: new FormControl(null, [Validators.required, Validators.maxLength(6)]),
+    inboundDeliveryStorageLocation: new FormControl(null, [Validators.maxLength(6)]),
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Flex display
+   */
+   public setFlex(flex: number) {
+    return '0 0 calc(800px * ' + flex + '/30)';
   }
 
 }
