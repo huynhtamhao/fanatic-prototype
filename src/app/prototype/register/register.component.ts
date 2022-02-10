2c336469de6aa9a69d4f99e4860936af26fe3f4b
Le Hoang Quan <quan-lh@fanatic.vn>
2022-02-10 18:12:53 +0700

    feat(form-register): use toast

--- src/app/prototype/register/register.component.ts
+++ src/app/prototype/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'kairos-register',
@@ -10,22 +11,43 @@ export class RegisterComponent implements OnInit {
 
   form: FormGroup = new FormGroup({
     productCd: new FormControl(null, [Validators.required, Validators.maxLength(20), Validators.minLength(6)]),
-    serialNo: new FormControl(null, [Validators.maxLength(20)]),
+    quantity: new FormControl(null, [Validators.pattern('^([0-9]+\.?[0-9]*|\.[0-9]+)$'), Validators.maxLength(6)]),
     invoiceNo: new FormControl(null, [Validators.required, Validators.maxLength(11)]),
     storageLocation: new FormControl(null, [Validators.required, Validators.maxLength(6)]),
     inboundDeliveryStorageLocation: new FormControl(null, [Validators.maxLength(6)]),
   });
-  
-  constructor() { }
+
+  constructor(
+    private toastrService: ToastrService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onRegister() {
+    // alert(this.form.controls['productCd'].value);
+    this.showSubmitMessage();
+  }
+
+  showSubmitMessage() {
+    // test toast
+    const index = Math.floor(Math.random() * 2);
+    if (index === 1) {
+      this.toastrService.info(messageSubmit[index]);
+    } else {
+      this.toastrService.error(messageSubmit[index]);
+    }
+  }
   /**
    * Flex display
    */
-   public setFlex(flex: number) {
+  public setFlex(flex: number) {
     return '0 0 calc(800px * ' + flex + '/30)';
   }
 
 }
+
+export const messageSubmit = [
+  "登録が完了しました。",
+  "登録が失敗しました。"
+]
