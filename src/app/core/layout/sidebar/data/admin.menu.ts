eaa8bf5afac9fb6b49c842dec7edd07e55f83b40
hao <huynhtamhao@gmail.com>
2022-02-15 16:50:50 +0700

    feat(menu): translate to japanese

--- src/app/core/layout/sidebar/data/admin.menu.ts
+++ src/app/core/layout/sidebar/data/admin.menu.ts
@@ -2,5 +2,10 @@ import { SidebarItem } from "./sidebar-item.metadata";
 
 // prettier-ignore
 export const ADMIN_MENU: SidebarItem[] = [
-
+  {
+    id         : '',
+    icon       : 'public',
+    path       : '',
+    submenu: []
+  }
 ]
