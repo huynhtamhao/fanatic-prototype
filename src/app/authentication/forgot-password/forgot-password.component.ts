044c77a3f2853a7ae04d9065978e2e859cd917e2
Le Hoang Quan <quan-lh@fanatic.vn>
2022-02-25 09:26:47 +0700

    feat(login): move css

--- src/app/authentication/forgot-password/forgot-password.component.ts
+++ src/app/authentication/forgot-password/forgot-password.component.ts
@@ -4,7 +4,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'kairos-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss', '../login/login.component.scss']
 })
 export class ForgotPasswordComponent {
 
